1ef1369a4596d09e16bd7667b6946941444ea996
ANUBHAV MAURYA <anubhawmaurya@gmail.com>
2022-09-17 11:38:49 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=10;
+
+        System.out.println("Product of a and b is :"+ (a*b));
+    }
+}
